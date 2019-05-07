3fc079f258fbb4755472b5e8d8bfed31872385e5
David <cichy.dawid@gmail.com>
2019-05-07 10:01:57 +0200

    Added aquare setters and getters

--- Square.java
+++ Square.java
@@ -1,36 +1,64 @@
-import java.awt.List;
-
-
 public class Square {
-    private int x;
-    private int y;
-    boolean isShipPart;
-
-    // isPartOfShip(Ship[] fleet){
-    //  List<Square> list = new List();
-    //     for (int i = 0; i < fleet.length; i++){
-        //      shipSize = listfleet[i].squareList().length();
-        //      for 
-    //          list += listfleet[i]
-    //     }
-    // }
-
+    private String state;
+    //(ship, ocean, miss, hit, neighbor, sank)
     public Square(int x, int y){
-        this.x = x;
-        this.y = y;
-        this.isShipPart = false;
+        this.state = "ocean";
     }
-
-    public String printingX() {
-        if (isShipPart){
-            return "X";
-        } else {
+    
+    public void makeShip(){
+        if (this.state.equals("ocean")){
+            this.state = "ship";
+        }
+    }
+    
+    public void makeNeighbor(){
+        if (this.state.equals("ocean")){
+            this.state = "neighbor";
+        }
+    }
+    
+    public void makeHit(){
+        if (this.state.equals("ship")){
+            this.state = "hit";
+        } else if (this.state.equals("ocean")) {
+            this.state = "miss";
+        }
+    }
+    
+    public void makeSunk(){
+        if (this.state.equals("ship")){
+            this.state = "sunk";
+        }
+    }
+    
+    
+    public String showStatusToOwner() {
+        //(ship,hit,sank, ocean, miss,  neighbor, )
+        if (this.state.equals("ship")){
+            return "■";
+        } else if (this.state.equals("hit")){
+            return "×";
+        }else if (this.state.equals("miss")){
+            return "·";
+        }
+        else {
             return " ";
          
         }
     }
 
-    public void setPartOfShip(){
-        this.isShipPart = true;
+    public String showStatusToOponent() {
+        //(ship,hit,sunk, ocean, miss,  neighbor, )
+        if (this.state.equals("sunk")){
+            return "■";
+        } else if (this.state.equals("hit")){
+            return "×";
+        }else if (this.state.equals("miss")){
+            return "·";
+        }
+        else {
+            return " ";
+         
+        }
     }
 }
