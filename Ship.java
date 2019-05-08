31da2c39f8e524956428deb0c2da7b45938ad2c9
David <cichy.dawid@gmail.com>
2019-05-08 11:24:54 +0200

    Added ship hit control

--- Ship.java
+++ Ship.java
@@ -1,11 +1,27 @@
+import java.util.Arrays;
+
 public class Ship {
     Square[] shipPosition;
+    boolean sunk;
 
     public Ship(Square[] shipPosition){
+        this.sunk = false;
         this.shipPosition = shipPosition;
         for (int i = 0; i < shipPosition.length; i++){
             shipPosition[i].makeShip();
         }
+    }
+
+    public boolean checkIfLocationIsPartoOfShip(Square location){
+        return Arrays.asList(this.shipPosition).contains(location);
+    }
+
+    public boolean tryToHitLocation(Square location){
+        boolean isHit = checkIfLocationIsPartoOfShip(location);
+        if (isHit){
+            location.makeHit();
+        }
+        return isHit;
     }
 
     // public Square[] squareList(){
