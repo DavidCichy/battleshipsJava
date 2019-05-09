780d882dca11d33d36d03d7427eb62262c066f9e
David <cichy.dawid@gmail.com>
2019-05-09 10:16:15 +0200

    Sunking ships

--- Ship.java
+++ Ship.java
@@ -18,7 +18,12 @@ public class Ship {
         for (int i = 0; i < this.shipPosition.length; i++){
             if (!this.shipPosition[i].isHit()){
                 this.sunk = false;
-            };}
+            }
+        if (this.sunk){
+            for (int j = 0; j < shipPosition.length; j++){
+                shipPosition[j].makeSunk();
+            }
+        };}
     }
 
     public boolean isSunk(){
